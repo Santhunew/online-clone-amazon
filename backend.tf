bdc6f411014a61fa78c3df751dce4df8ccf90f4b
Santhunew <118210988+Santhunew@users.noreply.github.com>
2025-02-06 22:58:59 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket-amazon-1"  # Change to match your S3 bucket
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-locks"
-  }
-}

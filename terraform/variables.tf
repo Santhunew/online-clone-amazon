93e243320b430cfa0c050e31c768811c411b135a
santhosh <santhosh.ss7878@gmail.com>
2025-02-06 23:19:51 +0530

    push-2

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "sg_name" {
+    description = "Name of the security group"
+    type = string
+    default     = "SG_App"  
+}
+
+variable "key_name" {
+    description = "Name of the key pair"
+    type = string
+    default     = "Santhu"
+}
